13aa887ac1d3fdb92d95d0b44e6675d2eca4d85b
creasyw <creasywuqiong@gmail.com>
2013-08-07 10:01:02 -0500

    sol of 1.35

---
+++ ch1/1.35.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (fixed-point f first-guess)
+  (letrec ((tolerance 0.00001))
+    (define (close-enough? v1 v2)
+      (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+      (let ((next (f guess)))
+        (if (close-enough? guess next)
+            next
+            (try next))))
+    (try first-guess)))
+
+;; calculate the golden ratio
+(fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
