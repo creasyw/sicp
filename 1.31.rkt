654d1fea41d86c08a0f1cd7ce88912afe856acbd
creasyw <creasywuqiong@gmail.com>
2013-08-06 19:12:11 -0500

    sol of 1.31

---
+++ 1.31.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(define (product term a next terminate)
+  (define (helper result acc)
+    (if (terminate acc) result
+        (helper (* result (term acc)) (next acc))))
+  (helper 1 a))
+
+(define (factorial n)
+  (product (lambda (x) (* x 1)) 1 (lambda (x) (+ x 1)) (lambda (x) (> x n))))
+
+(define (approximate-pi terms)
+  (define (next x)
+    (if (> x 1) (/ (numerator x) (+ 2 (denominator x)))
+        (/ (+ 2 (numerator x)) (denominator x))))
+  (letrec ((b (if (even? terms) (/ (+ terms 2) (+ terms 1))
+                  (/ (+ terms 1) (terms 2))))
+           (terminate (lambda (x) (> (numerator x) (numerator b)))))
+    (exact->inexact (* 4 (product (lambda (x) (* x 1)) 2/3 next terminate)))))
