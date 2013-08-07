6a80d537e73671d321797efb4595ab86c2559f41
creasyw <creasywuqiong@gmail.com>
2013-08-06 22:17:57 -0500

    sol of 1.32

---
+++ ch1/1.32.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+;; the last argument using "terminate" rather than b in the book
+;; is to find more general terminating criterion.
+(define (accumulator combiner null-value term start next terminate)
+  (define (helper result acc)
+    (if (terminate acc) result
+        (helper (combiner result (term acc)) (next acc))))
+  (helper null-value start))
+
+(define (sum term a next b)
+  (accumulator + 0 term a next (lambda (x) (> x b))))
+
+(define (product term a next b)
+  (accumulator * 1 term a next (lambda (x) (> x b))))
+
+;; test sum
+(sum (lambda (x) (+ x 0)) 1 (lambda (x) (+ x 1)) 10)
+
+;; test product
+(product (lambda (x) (+ x 0)) 1 (lambda (x) (+ x 1)) 10)
