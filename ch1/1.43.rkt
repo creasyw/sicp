a3a8f18c06326551ef74040551be259b87454538
creasyw <creasywuqiong@gmail.com>
2013-08-10 00:15:57 -0500

    modify api calling repeated

--- ch1/1.43.rkt
+++ ch1/1.43.rkt
@@ -1,11 +1,7 @@
 #lang racket
 
-(define (repeated f n)
-  (lambda (x)
-    (define (helper count)
-      (if (= count n) (f x)
-          (f (helper (+ count 1)))))
-     (helper 1)))
+;; import repeated
+(require "../procedure.rkt")
 
 (define (square x) (* x x))
 ((repeated square 2) 5)
