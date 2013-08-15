719c8d42f44b0e2675411039a559feba23467c17
creasyw <creasywuqiong@gmail.com>
2013-08-15 16:18:50 -0500

    sol of 2.19

---
+++ ch2/2.19.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (no-more? lst) (null? lst))
+(define (except-first-denomination lst) (cdr lst))
+(define (first-denomination lst) (car lst))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+;; test
+(cc 100 us-coins)
