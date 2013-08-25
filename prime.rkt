a1993d525ff59c60b6452e7793cb257e4311409d
creasyw <creasywuqiong@gmail.com>
2013-08-25 00:02:03 -0400

    change func

--- prime.rkt
+++ prime.rkt
@@ -10,6 +10,8 @@
                   (append (append (take new_lst index) '(0)) (drop new_lst (+ index 1)))))))
   (helper 1 lst))
 
+;; generate all prime numbers smaller than n
+;; n should be positive and larger than one
 (define (gen-prime n)
   (define (helper target ref)
     (cond ((null? ref) target)
@@ -22,7 +24,8 @@
     (drop (filter (lambda (x) (not (= 0 x)))
                   (helper candidates prime-range)) 1)))
 
-;; assuming the input n is larger than 1
+;; determine if a number is prime
+;; n should be positive and larger than one
 (define (prime? n)
   (let ((root (integer-sqrt n)))
     (define (helper i)
