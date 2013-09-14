893ef23573519dbfc9f12a319fe4e7226e0f31ef
creasyw <creasywuqiong@gmail.com>
2013-09-13 22:47:50 -0500

    Symbolic Differentiation

---
+++ ch2/symbolic_diff.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
