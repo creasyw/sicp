92ac621346718ffb6d1fbd6d7caef8e1249f13dd
creasyw <creasywuqiong@gmail.com>
2013-11-07 09:09:18 -0600

    basic derivative func

--- ch2/symbolic_diff.rkt
+++ ch2/symbolic_diff.rkt
@@ -4,6 +4,18 @@
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
+(define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-product a1 a2) (list '* a1 a2))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+(define (addend x) (cadr x))
+(define (augend s) (caddr s))
+(define (multiplier x) (cadr x))
+(define (multiplicand x) (caddr x))
+
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp)
@@ -19,3 +31,8 @@
                          (multiplicand exp))))
         (else
          (error "unknown expression type -- DERIV" exp))))
+
+;; test deriv
+(deriv '(+ x 3) 'x)
+(deriv '(* x y) 'x)
+(deriv '(* (* x y) (+ x 3)) 'x)
