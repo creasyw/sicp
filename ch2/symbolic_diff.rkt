711672038eb768ad66415fe04d8f8145545968eb
creasyw <creasywuqiong@gmail.com>
2013-11-07 09:30:11 -0600

    make the result simplest form

--- ch2/symbolic_diff.rkt
+++ ch2/symbolic_diff.rkt
@@ -4,8 +4,21 @@
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
-(define (make-sum a1 a2) (list '+ a1 a2))
-(define (make-product a1 a2) (list '* a1 a2))
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-sum a1 a2) 
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (#t (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (#t (list '* m1 m2))))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
