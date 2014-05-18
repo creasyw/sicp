62ddc8761e6d56e193c37b9f447ed06dc27fb9a8
creasyw <creasywuqiong@gmail.com>
2014-05-18 13:10:28 -0500

    sol of 2.58-1

---
+++ ch2/2.58.rkt
@@ -0,0 +1,51 @@
+#lang racket
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+;; constructor
+(define (make-sum a1 a2) 
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (#t (list a1 '+ a2))))
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (#t (list m1 '* m2))))
+
+;; predicate
+(define (sum? x)
+  (and (pair? x) (eq? (cadr x) '+)))
+(define (product? x)
+  (and (pair? x) (eq? (cadr x) '*)))
+;; selector
+(define (addend x) (car x))
+(define (augend s) (caddr s))
+(define (multiplier x) (car x))
+(define (multiplicand x) (caddr x))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+;; test deriv
+(deriv '(x + (3 * (x + (y + 2)))) `x)
