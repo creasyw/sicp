ad8da479d2baca5a112e704f79ab6eebd209bffa
creasyw <creasywuqiong@gmail.com>
2014-05-19 12:16:04 -0500

    reimplement using sort and iteration

--- ch2/2.60.rkt
+++ ch2/2.60.rkt
@@ -3,7 +3,19 @@
 (require (except-in  "2.59.rkt" adjoin-set))
 
 (define (build-set lst)
-  (set->list (list->set lst)))
+  (if (<= (length lst) 1)
+      lst
+      ;; use "larger than" here for increasing order output
+      (letrec ((sorted_lst (sort lst >)))
+        (define (helper rest acc)
+          (cond ((null? rest) acc)
+                ((equal? (car rest) (first acc))
+                 (helper (cdr rest) acc))
+                (#t (helper (cdr rest) (cons (car rest) acc)))))
+        (helper (cdr sorted_lst) (take sorted_lst 1)))))
+
+;; test build-set
+;(build-set (list 1 2 3 4 5 6 67 4 5 2 9))
 
 (define (adjoin-set x s1)
   (cons x s1))
