70c5ff517d242a4f688b91ba8a5ffe74cf0f6d65
creasyw <creasywuqiong@gmail.com>
2014-05-19 12:16:23 -0500

    sol of 2.60

--- ch2/2.60.rkt
+++ ch2/2.60.rkt
@@ -19,3 +19,21 @@
 
 (define (adjoin-set x s1)
   (cons x s1))
+
+;; Specifically for this question, without duplication, the complexities are
+;; O(n) for predicate, O(n) for adjoin, O(n^2) for union and intersection.
+;; If the duplicated items exit in the set, the corresponding complexities are
+;; O(n) for predicate, O(1) for adjoin, O(n^2) for union and intersection.
+;; However, if the N is large and there are many duplicated items in it, this
+;; implmentation would dramatically increase the space complexity and
+;; deteriorate the performance. (e.g. 90% duplicated, expected time 100x ).
+;; Meanwhile, it also complicate the implementations of union and intersection.
+
+;; Hence, it's better to add preprocessing and keep most of funcs as they are
+;; in the question 2.59. The most effective and time-saving way is:
+;; (define (build-set lst)
+;;   (set->list (list->set lst)))
+;; with O(logN) to build the set from list,
+;; though it seems "red herring" as using built-in set to implement set...
+;; The implemented method use O(NlogN) sorting and O(N) to eliminate duplicated
+;; items. So, the overall complexity for build-set is O(NlogN).
