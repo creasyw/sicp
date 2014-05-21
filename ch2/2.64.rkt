cef4e1da484008f5feae6847e9123631a6649d59
creasyw <creasywuqiong@gmail.com>
2014-05-21 11:43:29 -0500

    sol of 2.64 a)

--- ch2/2.64.rkt
+++ ch2/2.64.rkt
@@ -18,3 +18,11 @@
                (right-tree (car right-result))
                (remaining-elts (cdr right-result)))
         (cons (make-tree this-entry left-tree right-tree) remaining-elts))))
+
+;; a)
+;; In every level, the helper function "partial-tree" break the list
+;; into two (almost) even parts with length "left-size" and
+;; "right-size". Specifically for the operations, it first recursively
+;; builds the left-tree, and then from the deapest level of left-tree
+;; it completes each corresponding right-tree part. The current root
+;; will be returned as handler of the entire tree.
