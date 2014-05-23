7a05a882eb5b62d315a426633a26bd3a0be4ff96
creasyw <creasywuqiong@gmail.com>
2014-05-23 12:10:03 -0500

    implement huffman tree with ordered list

--- ch2/huffman-tree.rkt
+++ ch2/huffman-tree.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(provide all-defined-out)
+
 (define (make-leaf symbol weight)
   (list 'leaf symbol weight))
 (define (leaf? object)
@@ -26,3 +28,33 @@
       (weight-leaf tree)
       (cadddr tree)))
 
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (#t (error "bat bit -- CHOOSE-BRANCH" bit))))
+
+;; keep all of the symbol in-order according to its weight
+(define (adjoin-set x s)
+  (cond ((null? s) (list x))
+        ((< (weight x) (weight (car s))) (cons x s))
+        (#t (cons (car s)
+                  (adjoin-set x (cdr s))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair) (cadr pair)) ;(symbol, weight)
+                    (make-leaf-set (cdr pairs))))))
