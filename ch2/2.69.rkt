aec3511f39b06e57967ce9345a9426a50e857cbc
creasyw <creasywuqiong@gmail.com>
2014-05-24 17:30:39 -0500

    build successfully, but in a 'mirror' mode

---
+++ ch2/2.69.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(require "huffman-tree.rkt")
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge pairs)
+  (cond ((= 0 (length pairs)) '())
+        ((= 1 (length pairs)) (car pairs))
+        (#t (successive-merge (append (list (make-code-tree (car pairs)
+                                                            (cadr pairs)))
+                                      (drop pairs 2))))))
+
+;; testing
+(define lst (list '(A 4) '(B 2) '(C 1) '(D 1)))
+(define x (generate-huffman-tree lst))
+(displayln x)
+(displayln (left-branch x))
+(displayln (right-branch x))
