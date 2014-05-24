02b1e6c33ae4f4e625a73a81aa170863939f3216
creasyw <creasywuqiong@gmail.com>
2014-05-24 17:59:52 -0500

    use adjoin-set rather than simply append to make the set in-order

--- ch2/2.69.rkt
+++ ch2/2.69.rkt
@@ -8,9 +8,9 @@
 (define (successive-merge pairs)
   (cond ((= 0 (length pairs)) '())
         ((= 1 (length pairs)) (car pairs))
-        (#t (successive-merge (append (list (make-code-tree (car pairs)
-                                                            (cadr pairs)))
-                                      (drop pairs 2))))))
+        (#t (successive-merge (adjoin-set (make-code-tree (car pairs)
+                                                          (cadr pairs))
+                                          (drop pairs 2))))))
 
 ;; testing
 (define lst (list '(A 4) '(B 2) '(C 1) '(D 1)))
