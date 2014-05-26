6a78b6ae6a33230c85ea312176ff4bd34180b81d
creasyw <creasywuqiong@gmail.com>
2014-05-26 12:14:59 -0500

    huffman tree visualization

---
+++ ch2/2.71.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+;; import basic defs
+(require "huffman-tree.rkt")
+;; import generate-huffman-tree)
+(require "2.69.rkt")
+
+(define (print-tree tree)
+  (define (printing prefix tree)
+    (printf "~a Symbols:~a, Weight:~a\n."
+            prefix
+            (symbols tree)
+            (weight tree)))
+  (define (print-subtree prefix subtree)
+    (cond ((null? subtree) '())
+          ((leaf? subtree) (printing prefix subtree))
+          (#t (printing prefix subtree)
+              (print-subtree (string-append prefix "--") (left-branch subtree))
+              (print-subtree (string-append prefix "--") (right-branch subtree)))))
+
+  (print-subtree "--" tree))
+
+(print-tree (generate-huffman-tree
+             '((a 1) (b 2) (c 4) (d 8) (e 16))))
+
+(printf "\n\n")
+
+(print-tree
+ (generate-huffman-tree
+  '((a 1) (b 2) (c 4) (d 8 ) (e 16)
+    (f 32) (g 64) (h 128) (i 256) (j 512))))
