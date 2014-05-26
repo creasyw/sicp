514bc964ca785e9865a9282df6c7d47668115d2a
creasyw <creasywuqiong@gmail.com>
2014-05-26 12:17:23 -0500

    sol of 2.71

--- ch2/2.71.rkt
+++ ch2/2.71.rkt
@@ -29,3 +29,9 @@
  (generate-huffman-tree
   '((a 1) (b 2) (c 4) (d 8 ) (e 16)
     (f 32) (g 64) (h 128) (i 256) (j 512))))
+
+;; because the summation of all n-1 items is equal to the weights of
+;; the nth item, the tree will be "linear" -- in the sense that every
+;; level will have a leaf node and a subtree contains all items with
+;; smaller weights. As a result, the 1st node has length 1, and the
+;; last node has n-1 length ocde.
