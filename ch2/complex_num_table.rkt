06bae6e49ed2f08878e6dd73e88d81895030b1f1
creasyw <creasywuqiong@gmail.com>
2014-06-01 16:20:42 -0500

    change pair to mutable pair

--- ch2/complex_num_table.rkt
+++ ch2/complex_num_table.rkt
@@ -15,18 +15,19 @@
                   false))
             false)))
     (define (insert! key-1 key-2 value)
-      (let ((subtable (assoc key-1 (mcdr local-table))))
-        (if subtable
-            (let ((record (assoc key-2 (mcdr subtable))))
+      (let ((impair (assoc key-1 (mcdr local-table))))
+        (if impair
+            (letrec ((subtable (mcons (car impair) (cdr impair)))
+                     (record (assoc key-2 (mcdr subtable))))
               (if record
                   (set-mcdr! record value)
                   (set-mcdr! subtable
-                            (cons (cons key-2 value)
-                                  (mcdr subtable)))))
+                             (cons (cons key-2 value)
+                                   (mcdr subtable)))))
             (set-mcdr! local-table
-                      (cons (list key-1
-                                  (cons key-2 value))
-                            (mcdr local-table)))))
+                       (cons (list key-1
+                                   (cons key-2 value))
+                             (mcdr local-table)))))
       'ok)
     (define (dispatch m)
       (cond ((eq? m 'lookup-proc) lookup)
