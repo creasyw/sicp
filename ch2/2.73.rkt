f6800b98a4040af48f95d8877d1fa7fa38a0fee1
creasyw <creasywuqiong@gmail.com>
2014-06-01 17:03:41 -0500

    change func import according to the req. of the question

--- ch2/2.73.rkt
+++ ch2/2.73.rkt
@@ -6,11 +6,11 @@
 (require "complex_number.rkt")
 ;; import get, put
 (require "complex_num_table.rkt")
-;; import make-sum, make-product
-;; ... and all basic data symbols
-(require "symbolic_diff.rkt")
 
 (define (install-deriv-package)
+  
+  (define (=number? exp num)
+  (and (number? exp) (= exp num)))
   ;; the deriv-sum and deriv-product are actually the part of the
   ;; original deriv in the symbolic_diff.rkt. The reason to separate
   ;; them is to meet the interface provided by the question.
@@ -59,3 +59,17 @@
   (put 'deriv '** deriv-exponential)
   
   'done)
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ;; the "get" actually determines what to implement
+        (else ((get 'deriv (operator exp)) (operands exp)
+                                           var))))
+
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
