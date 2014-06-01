ec407d346a28dcd44edfc5ab978c223d2acf6286
creasyw <creasywuqiong@gmail.com>
2014-06-01 17:02:20 -0500

    works correct for mutable list operations

--- ch2/complex_num_table.rkt
+++ ch2/complex_num_table.rkt
@@ -7,26 +7,25 @@
 (define (make-table)
   (let ((local-table (mlist '*table*)))
     (define (lookup key-1 key-2)
-      (let ((subtable (assoc key-1 (mcdr local-table))))
+      (let ((subtable (massoc key-1 (mcdr local-table))))
         (if subtable
-            (let ((record (assoc key-2 (mcdr subtable))))
+            (let ((record (massoc key-2 (mcdr subtable))))
               (if record
                   (mcdr record)
                   false))
             false)))
     (define (insert! key-1 key-2 value)
-      (let ((impair (assoc key-1 (mcdr local-table))))
-        (if impair
-            (letrec ((subtable (mcons (car impair) (cdr impair)))
-                     (record (assoc key-2 (mcdr subtable))))
+      (let ((subtable (massoc key-1 (mcdr local-table))))
+        (if subtable
+            (letrec ((record (massoc key-2 (mcdr subtable))))
               (if record
                   (set-mcdr! record value)
                   (set-mcdr! subtable
-                             (cons (cons key-2 value)
+                             (mcons (mcons key-2 value)
                                    (mcdr subtable)))))
             (set-mcdr! local-table
-                       (cons (list key-1
-                                   (cons key-2 value))
+                       (mcons (mlist key-1
+                                   (mcons key-2 value))
                              (mcdr local-table)))))
       'ok)
     (define (dispatch m)
