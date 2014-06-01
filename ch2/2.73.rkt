9189a48923e5ee165143b894fcc8a6a295ed872f
creasyw <creasywuqiong@gmail.com>
2014-06-01 17:04:06 -0500

    rewrite deriv-sum and -product

--- ch2/2.73.rkt
+++ ch2/2.73.rkt
@@ -14,9 +14,25 @@
   ;; the deriv-sum and deriv-product are actually the part of the
   ;; original deriv in the symbolic_diff.rkt. The reason to separate
   ;; them is to meet the interface provided by the question.
+  (define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2)) (+ a1 a2))
+          (else (list '+ a1 a2))))
+  (define (addend opds) (car opds))
+  (define (augend opds) (cadr opds))
   (define (deriv-sum exp var)
     (make-sum (deriv (addend exp) var)
               (deriv (augend exp) var)))
+  
+  (define (make-product m1 m2)
+    (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+          ((=number? m1 1) m2)
+          ((=number? m2 1) m1)
+          ((and (number? m1) (number? m2)) (* m1 m2))
+          (else (list '* m1 m2))))
+  (define (multiplier opds) (car opds))
+  (define (multiplicand opds) (cadr opds))
   (define (deriv-product exp var)
     (make-sum
      (make-product (multiplier exp)
