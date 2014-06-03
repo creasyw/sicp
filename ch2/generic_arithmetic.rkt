26b2565b4833cd1bd2176a4ae02c99df7af2763f
creasyw <creasywuqiong@gmail.com>
2014-06-02 23:41:10 -0500

    basic operations for ordinary arithmetic

---
+++ ch2/generic_arithmetic.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+(define (install-number-package)
+  (define (tag x)
+    (attach-tag 'custom-number x))
+  (put 'add '(custom-number custom-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(custom-number custom-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(custom-number custom-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(custom-number custom-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'custom-number
+       (lambda (x) (tag x)))
+  'done)
