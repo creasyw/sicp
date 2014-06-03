9249a9e432e9027bde86df6ce77a5791248943be
creasyw <creasywuqiong@gmail.com>
2014-06-02 22:22:50 -0500

    sol for b)

--- ch2/2.74.rkt
+++ ch2/2.74.rkt
@@ -14,3 +14,17 @@
   (car generic-file))
 (define (original-file generic-file)
   (cdr generic-file))
+
+;; b)
+;; The generic record should contain the division name
+;; and the original record.
+(define (get-salary generic-record)
+    ((get 'get-salary (division generic-record))
+        (original-record generic-record)))
+
+(define (make-generic-record division record)
+    (cons division record))
+(define (division generic-record)
+    (car generic-record))
+(define (original-record generic-record)
+    (cdr generic-record))
