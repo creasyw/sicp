bff7fd0f9a08408484bf4253693f2f54d089bf21
creasyw <creasywuqiong@gmail.com>
2014-08-11 23:45:47 -0500

    sol of 2.77

--- ch2/2.77.rkt
+++ ch2/2.77.rkt
@@ -1,6 +1,14 @@
 #lang racket
 
 (require "generic_arithmetic.rkt")
+(install-complex-package)
 
-(define x (make-complex-from-mag-ang 10 1))
-(magnitude z)
+(apply-generic 'magnitude '(complex rectangular 3 . 4))
+;; 5
+
+;; The four lines of code given by A.P. Hacker build another table
+;; which has the real-part/imag-part/magnitude/angle in one axis and
+;; complex in the other axis. THis table will strip the first tag of
+;; the compound data. The second tag will determine which part of code
+;; will be excuted in the function of magnitude. Refer to
+;; generic_arithmetic.rkt for more details.
