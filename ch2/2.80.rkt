b59453eb35b32a7dc3214029f601f43f5df00c2b
creasyw <creasywuqiong@gmail.com>
2014-08-12 22:13:33 -0500

    testing =zero? for complex

--- ch2/2.80.rkt
+++ ch2/2.80.rkt
@@ -15,3 +15,10 @@
 (install-rational-package)
 (eq? #t (=zero? (make-rational 0 2)))
 (eq? #f (=zero? (make-rational 2 2)))
+
+(display "Testing the complex package...")
+(install-complex-package)
+(eq? #t (=zero? (make-complex-from-real-imag 0 0)))
+(eq? #t (=zero? (make-complex-from-mag-ang 0 0)))
+(eq? #t (=zero? (make-complex-from-mag-ang 0 1)))
+(eq? #f (=zero? (make-complex-from-mag-ang 1 0)))
