0107ad1078da14727afcfa0fecfe5dab62239aee
creasyw <creasywuqiong@gmail.com>
2014-08-14 16:50:07 -0500

    sol of 2.81

--- ch2/2.81.rkt
+++ ch2/2.81.rkt
@@ -2,3 +2,10 @@
 ;; Louis's solution would generate infinite loop if the "'proc" does
 ;; not define in the given package. On the other hand, if the "'proc"
 ;; has been defined, the same-type coercion will never be called.
+
+;; b.
+;; No. The original implementation of apply-generic is correct.
+;; Refer to the comments in generic_arithmetic.rkt for more info.
+
+;; c.
+;; Refer to generic_arithmetic.rkt for implementation.
