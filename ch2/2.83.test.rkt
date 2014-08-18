dac21a45b185df89227673e540149462b39d4eac
creasyw <creasywuqiong@gmail.com>
2014-08-18 08:44:57 -0500

    basic test for the function 'raise

---
+++ ch2/2.83.test.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+(require "2.83.rkt")
+
+(install-complex-package)
+(install-number-package)
+(install-rational-package)
+
+;; basic raise
+(apply-generic 'equ
+               (make-rational 10 1)
+               (apply-to-two 'raise (make-number 10)))
+(apply-generic 'equ
+               (make-number 5.0)
+               (apply-to-two 'raise (make-rational 10 2)))
+(apply-generic 'equ
+               (make-complex-from-real-imag 3.0 0)
+               (apply-to-two 'raise (make-number 3.0)))
+;; basic operation upone one or two arguments
+#;(apply-generic 'equ
+               (make-number 6)
+               (apply-generic 'mul (make-number 2) (make-number 3)))
+#;(apply-generic 'equ
+               (make-rational 1 10)
+               (apply-generic 'mul (make-rational 1 5) (make-rational 1 2)))
+
+;(apply-generic 'mul (make-number 5) (make-complex-from-mag-ang 2 2))
+;; operation upon multiple arguments
+#;(apply-generic 'equ
+               (make-rational 98 11)
+               (apply-generic 'add (make-number 5) (make-rational 10 11)(make-number 3)))
+#;(apply-generic 'equ
+               (make-rational 17 3)
+               (apply-generic 'add (make-rational 2 3) (make-rational 5 3) (make-rational 10 3)))
