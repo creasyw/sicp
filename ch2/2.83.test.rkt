2b3ca61a1772ed8e38116265aeeed794a5535041
creasyw <creasywuqiong@gmail.com>
2014-08-18 08:54:33 -0500

    test file

--- ch2/2.83.test.rkt
+++ ch2/2.83.test.rkt
@@ -17,18 +17,23 @@
                (make-complex-from-real-imag 3.0 0)
                (apply-to-two 'raise (make-number 3.0)))
 ;; basic operation upone one or two arguments
-#;(apply-generic 'equ
+(apply-generic 'equ
                (make-number 6)
                (apply-generic 'mul (make-number 2) (make-number 3)))
-#;(apply-generic 'equ
+(apply-generic 'equ
                (make-rational 1 10)
                (apply-generic 'mul (make-rational 1 5) (make-rational 1 2)))
+(apply-generic 'equ
+               (make-complex-from-mag-ang 10.0 2.0)
+               (apply-generic 'mul (make-number 5) (make-complex-from-mag-ang 2 2)))
 
-;(apply-generic 'mul (make-number 5) (make-complex-from-mag-ang 2 2))
 ;; operation upon multiple arguments
-#;(apply-generic 'equ
+(apply-generic 'equ
                (make-rational 98 11)
                (apply-generic 'add (make-number 5) (make-rational 10 11)(make-number 3)))
-#;(apply-generic 'equ
+(apply-generic 'equ
                (make-rational 17 3)
                (apply-generic 'add (make-rational 2 3) (make-rational 5 3) (make-rational 10 3)))
+(apply-generic 'equ
+               (make-complex-from-real-imag 18.0 3.0)
+               (apply-generic 'mul (make-rational 3 1) (make-number 1) (make-complex-from-real-imag 6 1)))
