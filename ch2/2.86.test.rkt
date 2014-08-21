380f1e735df493f6c5e46ecd34507e5856c55941
creasyw <creasywuqiong@gmail.com>
2014-08-21 16:14:01 -0500

    add test cases for rational package

--- ch2/2.86.test.rkt
+++ ch2/2.86.test.rkt
@@ -5,10 +5,14 @@
 (install-rational-package)
 (install-complex-package)
 
-
 ;; testing custom-number package
 (= (+ 5 1) (add (make-number 5) (make-number 1)))
 (= (sqrt 10) (square-root (make-number 10)))
 (= (sin 2) (sine (make-number 2)))
 (= (cos 2) (cosine (make-number 2)))
 (= (atan 2 2) (atangent 2 2))
+
+;; testing rational package
+(= (+ 5/2 3/2) (add (make-rational 5 2) (make-rational 3 2)))
+(equ? (make-rational 4 3) (square-root (make-rational 16 9)))
+(= (sqrt 4/3) (square-root (make-rational 4 3)))
