19db6e1e7ea7213a6fc6902d5a742076901f401c
creasyw <creasywuqiong@gmail.com>
2014-08-23 23:04:24 -0500

    basic package for polynomial

--- ch2/2.87.rkt
+++ ch2/2.87.rkt
@@ -15,3 +15,28 @@
                             (term-list p2)))
       (error "Polys not in same var -- MUL-POLY"
              (list p1 p2))))
+
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+  ;; procedures same-variable? and variable? from section 2.3.2
+  ;; representation of terms and term lists
+  ;; procedures adjoin-term ...coeff from text below
+
+  (define (add-poly p1 p2) ...)
+  <procedures used by add-poly>
+  (define (mul-poly p1 p2) ...)
+  <procedures used by mul-poly>
+  ;; interface to rest of the system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  'done)
