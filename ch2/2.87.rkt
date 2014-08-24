dc7f159736a24bdfc20c6471ea3804ce87326ac9
creasyw <creasywuqiong@gmail.com>
2014-08-23 23:06:01 -0500

    add basic functions for add and mul

--- ch2/2.87.rkt
+++ ch2/2.87.rkt
@@ -1,5 +1,24 @@
 #lang racket
 
+;; for add
+(define (add-terms L1 L2)
+  (cond ((empty-termlist? L1) L2)
+        ((empty-termlist? L2) L1)
+        (else
+         (let ((t1 (first-term L1)) (t2 (first-term L2)))
+           (cond ((> (order t1) (order t2))
+                  (adjoin-term
+                   t1 (add-terms (rest-terms L1) L2)))
+                 ((< (order t1) (order t2))
+                  (adjoin-term
+                   t2 (add-terms L1 (rest-terms L2))))
+                 (else
+                  (adjoin-term
+                   (make-term (order t1)
+                              (add (coeff t1) (coeff t2)))
+                   (add-terms (rest-terms L1)
+                              (rest-terms L2)))))))))
+
 (define add-poly p1 p2
   (if (same-variable? (variable p1) (variable p2))
       (make-poly (variable p1)
@@ -7,6 +26,21 @@
                             (term-list p2)))
       (error "Poly not in same var -- ADD-POLY"
              (list p1 p2))))
+
+;; for multiplication
+(define (mul-terms L1 L2)
+  (if (empty-termlist? L1)
+      (the-empty-termlist)
+      (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                 (mul-terms (rest-terms L1) L2))))
+(define (mul-term-by-all-terms t1 L)
+  (if (empty-termlist? L)
+      (the-empty-termlist)
+      (let ((t2 (first-term L)))
+        (adjoin-term
+         (make-term (+ (order t1) (order t2))
+                    (mul (coeff t1) (coeff t2)))
+         (mul-term-by-all-terms t1 (rest-terms L))))))
 
 (define (mul-poly p1 p2)
   (if (same-variable? (variable p1) (variable p2))
