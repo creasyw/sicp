2f22578bbb776da0e8a7504a9923d6e9f61303db
creasyw <creasywuqiong@gmail.com>
2014-08-24 22:01:32 -0500

    test =zero?

--- ch2/2.87.test.rkt
+++ ch2/2.87.test.rkt
@@ -6,6 +6,10 @@
 
 (define ca (make-polynomial 'x '((2 2) (1 3) (0 1))))
 (define cb (make-polynomial 'x '((3 4) (1 1))))
+(define cc (make-polynomial 'x '((1 1) (0 10))))
+(define cd (make-polynomial 'x '((3 -4) (1 -1))))
 
 (add ca cb)
 ; (polynomial x (3 4) (2 2) (1 4) (0 1))
+(=zero? (add cb cd))
+; #t
