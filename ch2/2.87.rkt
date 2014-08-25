ea36382aa8778f9390170a10a87ced41f469fc11
creasyw <creasywuqiong@gmail.com>
2014-08-24 21:28:10 -0500

    unify all funcs into package

--- ch2/2.87.rkt
+++ ch2/2.87.rkt
@@ -1,54 +1,9 @@
 #lang racket
 
-;; for add
-(define (add-terms L1 L2)
-  (cond ((empty-termlist? L1) L2)
-        ((empty-termlist? L2) L1)
-        (else
-         (let ((t1 (first-term L1)) (t2 (first-term L2)))
-           (cond ((> (order t1) (order t2))
-                  (adjoin-term
-                   t1 (add-terms (rest-terms L1) L2)))
-                 ((< (order t1) (order t2))
-                  (adjoin-term
-                   t2 (add-terms L1 (rest-terms L2))))
-                 (else
-                  (adjoin-term
-                   (make-term (order t1)
-                              (add (coeff t1) (coeff t2)))
-                   (add-terms (rest-terms L1)
-                              (rest-terms L2)))))))))
+;; import get, put
+(require "complex_num_table.rkt")
 
-(define add-poly p1 p2
-  (if (same-variable? (variable p1) (variable p2))
-      (make-poly (variable p1)
-                 (att-terms (term-list p1)
-                            (term-list p2)))
-      (error "Poly not in same var -- ADD-POLY"
-             (list p1 p2))))
-
-;; for multiplication
-(define (mul-terms L1 L2)
-  (if (empty-termlist? L1)
-      (the-empty-termlist)
-      (add-terms (mul-term-by-all-terms (first-term L1) L2)
-                 (mul-terms (rest-terms L1) L2))))
-(define (mul-term-by-all-terms t1 L)
-  (if (empty-termlist? L)
-      (the-empty-termlist)
-      (let ((t2 (first-term L)))
-        (adjoin-term
-         (make-term (+ (order t1) (order t2))
-                    (mul (coeff t1) (coeff t2)))
-         (mul-term-by-all-terms t1 (rest-terms L))))))
-
-(define (mul-poly p1 p2)
-  (if (same-variable? (variable p1) (variable p2))
-      (make-poly (variable p1)
-                 (mul-terms (term-list p1)
-                            (term-list p2)))
-      (error "Polys not in same var -- MUL-POLY"
-             (list p1 p2))))
+(provide (all-defined-out))
 
 (define (install-polynomial-package)
   ;; internal procedures
@@ -64,18 +19,68 @@
     (if (=zero? (coeff term))
         term-list
         (cons term term-list)))
+
+  ;; predicate
+  (define (empty-termlist? term-list) (null? term-list))
+  ;; constructor
   (define (the-empty-termlist) '())
   (define (first-term term-list) (car term-list))
-  (define (rest-terms term-list) (cdr term-list))
-  (define (empty-termlist? term-list) (null? term-list))
   (define (make-term order coeff) (list order coeff))
+  ;; selectors
+  (define (rest-terms term-list) (cdr term-list))
   (define (order term) (car term))
   (define (coeff term) (cadr term))
 
-  (define (add-poly p1 p2) ...)
-  <procedures used by add-poly>
-  (define (mul-poly p1 p2) ...)
-  <procedures used by mul-poly>
+  ;; for add
+  (define add-poly p1 p2
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (add-terms (term-list p1)
+                              (term-list p2)))
+        (error "Poly not in same var -- ADD-POLY"
+               (list p1 p2))))
+
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+           (let ((t1 (first-term L1)) (t2 (first-term L2)))
+             (cond ((> (order t1) (order t2))
+                    (adjoin-term
+                     t1 (add-terms (rest-terms L1) L2)))
+                   ((< (order t1) (order t2))
+                    (adjoin-term
+                     t2 (add-terms L1 (rest-terms L2))))
+                   (else
+                    (adjoin-term
+                     (make-term (order t1)
+                                (add (coeff t1) (coeff t2)))
+                     (add-terms (rest-terms L1)
+                                (rest-terms L2)))))))))
+
+  ;; for multiplication
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (mul-terms (term-list p1)
+                              (term-list p2)))
+        (error "Polys not in same var -- MUL-POLY"
+               (list p1 p2))))
+
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                   (mul-terms (rest-terms L1) L2))))
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        (the-empty-termlist)
+        (let ((t2 (first-term L)))
+          (adjoin-term
+           (make-term (+ (order t1) (order t2))
+                      (mul (coeff t1) (coeff t2)))
+           (mul-term-by-all-terms t1 (rest-terms L))))))
+
   ;; interface to rest of the system
   (define (tag p) (attach-tag 'polynomial p))
   (put 'add '(polynomial polynomial)
