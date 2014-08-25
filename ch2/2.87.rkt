1b1be63286751ea5862c82c8424d75fae1a745ce
creasyw <creasywuqiong@gmail.com>
2014-08-24 21:50:50 -0500

    add apply-generic to use the package

--- ch2/2.87.rkt
+++ ch2/2.87.rkt
@@ -116,3 +116,12 @@
   (if (number? contents)
       contents
       (cons type-tag contents)))
+
+;; interface for the package
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these types"
+                 (list op type-tags))))))
