90da7f8d45bce7d87516b7dd6c5fbcfdbebe5116
creasyw <creasywuqiong@gmail.com>
2014-08-27 15:39:12 -0500

    add dense representation and distinguish it from sparse one

--- ch2/2.90.rkt
+++ ch2/2.90.rkt
@@ -12,7 +12,7 @@
 (define (equ? x y) (apply-generic 'equ? x y))
 (define (=zero? x) (apply-generic '=zero? x))
 
-(define (install-polynomial-package)
+(define (install-sparse-package)
   ;; internal procedures
   ;; representation of poly
   (define (make-poly variable term-list)
@@ -93,14 +93,103 @@
            (mul-term-by-all-terms t1 (rest-terms L))))))
 
   ;; interface to rest of the system
-  (define (tag p) (attach-tag 'polynomial p))
-  (put 'add '(polynomial polynomial)
+  (define (tag p) (attach-tag 'sparse p))
+  (put 'add '(sparse sparse)
        (lambda (p1 p2) (tag (add-poly p1 p2))))
-  (put 'sub '(polynomial polynomial)
+  (put 'sub '(sparse sparse)
        (lambda (p1 p2) (tag (add-poly p1 (negate p2)))))
-  (put 'mul '(polynomial polynomial)
+  (put 'mul '(sparse sparse)
        (lambda (p1 p2) (tag (mul-poly p1 p2))))
-  (put '=zero? '(polynomial)
+  (put '=zero? '(sparse)
+       (lambda (p) (empty-termlist? (term-list p))))
+  (put 'make 'sparse
+       (lambda (var terms) (tag (make-poly var terms))))
+  'done)
+
+;; for dense representation
+(define (install-dense-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (variable? p) (symbol? p))
+  (define (term-list p) (cdr p))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+  ;; predicate
+  (define (empty-termlist? term-list) (null? term-list))
+  ;; constructor and selector for "sparse order of polynomial"
+  (define (the-empty-termlist) '())
+  (define (make-term order coeff) (list order coeff))
+  (define (first-term term-list) (list (- (length term-list) 1) (car term-list)))
+  (define (rest-terms term-list) (cdr term-list))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+
+  ;; for add
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (add-terms (term-list p1)
+                              (term-list p2)))
+        (error "Poly not in same var -- ADD-POLY"
+               (list p1 p2))))
+  ;; helper for add-poly
+  (define (add-terms L1 L2)
+    (define (list-add lst1 lst2)
+      (for/list ([l1 lst1] [l2 lst2])
+        (+ l1 l2)))
+    (define (remove-leading-zeros lst)
+      (cond ((null? lst) lst)
+            ((= 0 (car lst)) (remove-leading-zeros (cdr lst)))
+            (#t lst)))
+    (define (helper)
+      (cond ((empty-termlist? L1) L2)
+            ((empty-termlist? L2) L1)
+            (#t (letrec ((len1 (length L1))
+                         (len2 (length L2)))
+                  (cond ((> len1 len2) (append (take L1 (- len1 len2))
+                                               (list-add (drop L1 (- len1 len2)) L2)))
+                        ((< len1 len2) (append (take L2 (- len2 len1))
+                                               (list-add L1 (drop L2 (- len2 len1)))))
+                        (#t (list-add L1 L2))))))  )
+    (remove-leading-zeros (helper)))
+
+  ;; for subtraction
+  (define (negate p1)
+    (mul-poly p1 (make-poly (variable p1) '(-1))))
+
+  ;; for multiplication
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (mul-terms (term-list p1)
+                              (term-list p2)))
+        (error "Polys not in same var -- MUL-POLY"
+               (list p1 p2))))
+  ;; helper function for mul-pily
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                   (mul-terms (rest-terms L1) L2))))
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        (the-empty-termlist)
+        (let ((newlist (append L (build-list (order t1) (lambda (x) 0)))))
+          (map (lambda (x) (* x (coeff t1))) newlist))))
+
+  ;; interface to rest of the system
+  (define (tag p) (attach-tag 'dense p))
+  (put 'add '(dense dense)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'sub '(dense dense)
+       (lambda (p1 p2) (tag (add-poly p1 (negate p2)))))
+  (put 'mul '(dense dense)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put '=zero? '(dense)
        (lambda (p) (empty-termlist? (term-list p))))
   (put 'make 'polynomial
        (lambda (var terms) (tag (make-poly var terms))))
