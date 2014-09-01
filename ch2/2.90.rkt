8f349943f5ab2d46711e31d8a7d47bc33ca06ab7
creasyw <creasywuqiong@gmail.com>
2014-09-01 14:52:02 -0500

    sol of 2.90

--- ch2/2.90.rkt
+++ ch2/2.90.rkt
@@ -234,6 +234,7 @@
       ((get 'make 'sparse) var terms)
       ((get 'make 'dense) var terms)))
 
+
 ;; ordinary number
 ;; tag: custom-number
 (define (install-number-package)
@@ -292,30 +293,3 @@
     (if proc
         (apply proc (map contents new-args))
         (error "No method for these types" (list op type-tags)))))
-
-;; for testing
-(install-sparse-package)
-(install-dense-package)
-(install-number-package)
-(make-polynomial 'x '(2 3 1))
-(make-polynomial 'y '((2 2) (1 3) (0 1)))
-
-(define ca (make-polynomial 'x '((2 2) (1 3) (0 1))))
-(define cb (make-polynomial 'x '((3 4) (1 1))))
-(define cc (make-polynomial 'x '((1 1) (0 10))))
-(define cd (make-polynomial 'x '(4 0 1 0)))
-
-
-(displayln "\nAdding two sparse together:")
-(add ca cb)
-; '(sparse x (3 4) (2 2) (1 4) (0 1))
-(add cd cd)
-; '(dense x 8 0 2 0)
-(mul ca cb)
-; '(sparse x (5 8) (4 12) (3 6) (2 3) (1 1))
-(mul cd cd)
-; '(dense x 16 0 8 0 1 0 0)
-
-(displayln "\nAdding a sparse with dense:")
-(add cd ca)
-; '(dense x 4 2 4 1)
