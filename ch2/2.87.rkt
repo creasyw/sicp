e6be53b06d3f8037e951fd553cfe61f88dadbd7a
creasyw <creasywuqiong@gmail.com>
2014-09-03 11:17:24 -0500

    implement div-poly

--- ch2/2.87.rkt
+++ ch2/2.87.rkt
@@ -91,6 +91,30 @@
                       (mul (coeff t1) (coeff t2)))
            (mul-term-by-all-terms t1 (rest-terms L))))))
 
+  (define (div-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1) (div-terms (term-list p1) (term-list p2)))
+        (error "Poly not in same var -- DIV-POLY" (list p1 p2))))
+  (define (div-terms L1 L2)
+    (if (empty-termlist? L1)
+        (list (the-empty-termlist) (the-empty-termlist))
+        (let ((t1 (first-term L1))
+              (t2 (first-term L2)))
+          (if (> (order t2) (order t1))
+              (list (the-empty-termlist) L1)
+              (let ((new-c (/ (coeff t1) (coeff t2)))
+                    (new-o (- (order t1) (order t2))))
+                (let ((rest-of-result
+                       (add-terms L1
+                                  (mul-terms '((0 -1))
+                                             (mul-terms (list (list new-o new-c)) L2)))
+                       ))
+                  (list (adjoin-term
+                         (make-term new-o new-c)
+                         (car (div-terms rest-of-result L2)))
+                        (cadr (div-terms rest-of-result L2)))
+                  ))))))
+
   ;; interface to rest of the system
   (define (tag p) (attach-tag 'polynomial p))
   (put 'add '(polynomial polynomial)
