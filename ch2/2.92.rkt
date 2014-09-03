4b3b29be6a420610ab283d2a28d53505a53c0c88
creasyw <creasywuqiong@gmail.com>
2014-09-03 17:03:01 -0500

    basic verison

---
+++ ch2/2.92.rkt
@@ -0,0 +1,197 @@
+#lang racket
+
+;; import get, put
+(require "complex_num_table.rkt")
+
+(provide (all-defined-out))
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+(define (=zero? x) (apply-generic '=zero? x))
+
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (variable? p) (symbol? p))
+  (define (term-list p) (cdr p))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
+  ;; representation of terms and term lists
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+        term-list
+        (cons term term-list)))
+
+  ;; predicate
+  (define (empty-termlist? term-list) (null? term-list))
+  ;; constructor and selector for "sparse order of polynomial"
+  (define (the-empty-termlist) '())
+  (define (make-term order coeff) (list order coeff))
+  (define (first-term term-list) (car term-list))
+  (define (rest-terms term-list) (cdr term-list))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+
+  ;; for add
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (add-terms (term-list p1)
+                              (term-list p2)))
+        (error "Poly not in same var -- ADD-POLY"
+               (list p1 p2))))
+  ;; helper for add-poly
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+           (let ((t1 (first-term L1)) (t2 (first-term L2)))
+             (cond ((> (order t1) (order t2))
+                    (adjoin-term
+                     t1 (add-terms (rest-terms L1) L2)))
+                   ((< (order t1) (order t2))
+                    (adjoin-term
+                     t2 (add-terms L1 (rest-terms L2))))
+                   (else
+                    (adjoin-term
+                     (make-term (order t1)
+                                (add (coeff t1) (coeff t2)))
+                     (add-terms (rest-terms L1)
+                                (rest-terms L2)))))))))
+
+  ;; for subtraction
+  (define (negate p1)
+    (mul-poly p1 (make-poly (variable p1) '((0 -1)))))
+
+  ;; for multiplication
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (mul-terms (term-list p1)
+                              (term-list p2)))
+        (error "Polys not in same var -- MUL-POLY"
+               (list p1 p2))))
+  ;; helper function for mul-pily
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                   (mul-terms (rest-terms L1) L2))))
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        (the-empty-termlist)
+        (let ((t2 (first-term L)))
+          (adjoin-term
+           (make-term (+ (order t1) (order t2))
+                      (mul (coeff t1) (coeff t2)))
+           (mul-term-by-all-terms t1 (rest-terms L))))))
+
+  (define (div-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (letrec ((two-lst (div-terms (term-list p1) (term-list p2)))
+                 (quo-lst (car two-lst))
+                 (rem-lst (cadr two-lst)))
+          (list (make-poly (variable p1) quo-lst)
+                (make-poly (variable p1) rem-lst)))
+        (error "Poly not in same var -- DIV-POLY" (list p1 p2))))
+
+  (define (div-terms L1 L2)
+    (if (empty-termlist? L1)
+        (list (the-empty-termlist) (the-empty-termlist))
+        (letrec ((t1 (first-term L1))
+                 (t2 (first-term L2)))
+          (if (> (order t2) (order t1))
+              (list (the-empty-termlist) L1)
+              (letrec ((new-c (div (coeff t1) (coeff t2)))
+                       (new-o (sub (order t1) (order t2)))
+                       (rest-of-result
+                        (add-terms L1
+                                   (mul-terms '((0 -1))
+                                              (mul-terms (list (list new-o new-c)) L2)))))
+                (list (adjoin-term
+                       (make-term new-o new-c)
+                       (car (div-terms rest-of-result L2)))
+                      (cadr (div-terms rest-of-result L2))))))))
+
+  ;; interface to rest of the system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'sub '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 (negate p2)))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put '=zero? '(polynomial)
+       (lambda (p) (empty-termlist? (term-list p))))
+  ;; because there are two polynomials coming out of the div-terms, it
+  ;; has to attach variable and 'polynomial twice in both div-poly and here.
+  (put 'div '(polynomial polynomial)
+       (lambda (p1 p2) (letrec ((two-poly (div-poly p1 p2))
+                                    (quo-poly (car two-poly))
+                                    (rem-poly (cadr two-poly)))
+                             (list (tag quo-poly) (tag rem-poly)))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  'done)
+
+(define (make-polynomial var terms)
+    ((get 'make 'polynomial) var terms))
+
+;; ordinary number
+;; tag: custom-number
+(define (install-number-package)
+  (define (tag x)
+    (attach-tag 'custom-number x))
+  (put 'add '(custom-number custom-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(custom-number custom-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(custom-number custom-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(custom-number custom-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'square-root '(custom-number)
+       (lambda (x) (tag (sqrt x))))
+  (put 'sine '(custom-number)
+       (lambda (x) (tag (sin x))))
+  (put 'cosine '(custom-number)
+       (lambda (x) (tag (cos x))))
+  (put 'atangent '(custom-number custom-number)
+       (lambda (x y) (tag (atan x y))))
+  (put 'equ '(custom-number custom-number) =)
+  (put '=zero? '(custom-number)
+       (lambda (x) (= x 0)))
+  (put 'make 'custom-number
+       (lambda (x) (tag x)))
+  'done)
+;; constructor.
+(define (make-number n)
+  ((get 'make 'custom-number) n))
+
+;; tagging functions
+(define (type-tag datum)
+  (cond ((pair? datum) (car datum))
+        ((number? datum) 'custom-number)
+        (#t (error "Bad tagged datum -- TYPE-TAG" datum))))
+(define (contents datum)
+  (cond ((pair? datum) (cdr datum))
+        ((number? datum) datum)
+        (#t (error "Bad tagged datum -- CONTENTS" datum))))
+(define (attach-tag type-tag contents)
+  (if (number? contents)
+      contents
+      (cons type-tag contents)))
+
+;; interface for the package
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these types"
+                 (list op type-tags))))))
