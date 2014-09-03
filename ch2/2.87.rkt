f805099c4dad73bf1187e70d563facfa9215c2e8
creasyw <creasywuqiong@gmail.com>
2014-09-03 11:17:05 -0500

    refine interface

--- ch2/2.87.rkt
+++ ch2/2.87.rkt
@@ -9,7 +9,6 @@
 (define (sub x y) (apply-generic 'sub x y))
 (define (mul x y) (apply-generic 'mul x y))
 (define (div x y) (apply-generic 'div x y))
-(define (equ? x y) (apply-generic 'equ? x y))
 (define (=zero? x) (apply-generic '=zero? x))
 
 (define (install-polynomial-package)
