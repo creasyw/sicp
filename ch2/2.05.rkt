1d1311fe68d779764e28cbbfd1dd544b8119d0b7
Qiong Wu <creasywuqiong@gmail.com>
2016-01-03 12:16:13 -0800

    sol of 2.05

---
+++ ch2/2.05.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(define (cons2 a b)
+  (lambda (selector)
+    (selector (* (expt 2 a) (expt 3 b)))))
+
+(define (car2 z)
+  (z (lambda (num)
+       (define (helper temp acc)
+         (if (> (modulo temp 2) 0) acc
+             (helper (/ temp 2) (+ acc 1))))
+       (helper num 0))))
+
+(define (cdr2 z)
+  (z (lambda (num)
+       (define (helper temp acc)
+         (if (> (modulo temp 3) 0) acc
+             (helper (/ temp 3) (+ acc 1))))
+       (helper num 0))))
+
+
+;; test
+(define x (cons2 5 7))
+(eq? (car2 x) 5)
+(eq? (cdr2 x) 7)
