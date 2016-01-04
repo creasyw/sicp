43d1b2974c7c2f484840557b4130aefa2fad86bb
Qiong Wu <creasywuqiong@gmail.com>
2016-01-03 20:27:04 -0800

    Update mul and div

--- ch2/2.07.rkt
+++ ch2/2.07.rkt
@@ -22,21 +22,28 @@
   (make-interval (add (lower-bound x) (lower-bound y))
                  (add (upper-bound x) (upper-bound y))))
 
+(define mult
+  (lambda (x y) (round-off (* x y) tolerance)))
+
 (define (mul-interval x y)
-  (let ((p1 (* (lower-bound x) (lower-bound y)))
-        (p2 (* (lower-bound x) (upper-bound y)))
-        (p3 (* (upper-bound x) (lower-bound y)))
-        (p4 (* (upper-bound x) (upper-bound y))))
+  (let ((p1 (mult (lower-bound x) (lower-bound y)))
+        (p2 (mult (lower-bound x) (upper-bound y)))
+        (p3 (mult (upper-bound x) (lower-bound y)))
+        (p4 (mult (upper-bound x) (upper-bound y))))
     (make-interval (min p1 p2 p3 p4)
                    (max p1 p2 p3 p4))))
 ;; 2.10
+
+(define div
+  (lambda (x y) (round-off (/ x y) tolerance)))
+
 (define (div-interval x y)
   (if (or (= 0 (upper-bound y))
           (= 0 (lower-bound y)))
       (displayln "y might not be divisible")
       (mul-interval x
-                    (make-interval (/ 1.0 (upper-bound y))
-                                   (/ 1.0 (lower-bound y))))))
+                    (make-interval (div 1.0 (upper-bound y))
+                                   (div 1.0 (lower-bound y))))))
 
 ;; 2.08
 (define sub
