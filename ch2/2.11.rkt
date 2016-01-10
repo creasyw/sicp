f14f5e8e815950e0c5928e58f19b4f1a431c4e40
Qiong Wu <creasywuqiong@gmail.com>
2016-01-10 15:08:59 -0800

    Correct procedure

--- ch2/2.11.rkt
+++ ch2/2.11.rkt
@@ -1,24 +1,35 @@
 #lang racket
 
 (require "2.07.rkt")
-
+0
 (define (mul-interval2 x y)
   (cond ((> (upper-bound y) (upper-bound x))
          (mul-interval2 y x))
-        ;; In addition to the condition above, there are three
-        ;; premises below, upper is larger than lower for each
-        ;; interval, and (upper x) larger than (upper y). The left
-        ;; independent variable is between (lower x) and (upper y).
-        ((and (> (lower-bound x) 0) (> (lower-bound y) 0))
-         (make-interval (mult (lower-bound x) (lower-bound y))
-                        (mult (upper-bound x) (upper-bound y))))
-        ((and (or (> 0 (lower-bound x)) (> 0 (lower-bound y)))
-              (> (upper-bound x) 0))
-         (make-interval (mult (upper-bound x) (min (lower-bound x) (lower-bound y)))
-                        (mult (upper-bound x) (max (lower-bound x) (upper-bound y)))))
-        ((> 0 (upper-bound x))
-         (make-interval (mult (upper-bound x) (max (lower-bound x) (upper-bound y)))
-                        (mult (min (lower-bound x) (upper-bound y)) (lower-bound y))))
+        ((and (>= (upper-bound x) 0) (>= (upper-bound y) 0))
+         (cond ((and (>= (lower-bound x) 0) (>= (lower-bound y) 0))
+                (make-interval (mult (lower-bound x) (lower-bound y))
+                               (mult (upper-bound x) (upper-bound y))))
+               ((and (>= (lower-bound x) 0) (< (lower-bound y) 0))
+                (make-interval (mult (upper-bound x) (lower-bound y))
+                               (mult (upper-bound x) (upper-bound y))))
+               ((and (< (lower-bound x) 0) (>= (lower-bound y) 0))
+                (make-interval (mult (lower-bound x) (upper-bound y))
+                               (mult (upper-bound x) (upper-bound y))))
+               ((and (< (lower-bound x) 0) (< (lower-bound y) 0))
+                (make-interval (min (mult (upper-bound x) (lower-bound y))
+                                    (mult (upper-bound y) (lower-bound x)))
+                               (max (mult (upper-bound x) (upper-bound y))
+                                    (mult (lower-bound x) (lower-bound y)))))))
+        ((and (>= (upper-bound x) 0) (< (upper-bound y) 0))
+         (cond ((>= (lower-bound x) 0)
+                (make-interval (mult (upper-bound x) (lower-bound y))
+                               (mult (lower-bound x) (upper-bound y))))
+               (else
+                (make-interval (mult (upper-bound x) (lower-bound y))
+                               (mult (lower-bound x) (lower-bound y))))))
+        ((and (< (upper-bound x) 0) (< (upper-bound y) 0))
+         (make-interval (mult (upper-bound x) (upper-bound y))
+                        (mult (lower-bound x) (lower-bound y))))
         (else (displayln "Should not be here!!!"))))
 
 (define eq-interval?
