683696c957eed984c98344c98afcd5259dcc079c
Qiong Wu <creasywuqiong@gmail.com>
2016-01-28 16:12:01 -0800

    sol of 2.15

---
+++ ch2/2.15.rkt
@@ -0,0 +1,4 @@
+;; Her answer is right. Basically, every operation would increase the
+;; "uncertainty". As a result, if we can simplify the representation
+;; of the formula first and then solve the intervals, we're able to
+;; have the tightest percentage.
