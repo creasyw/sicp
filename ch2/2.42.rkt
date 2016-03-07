d8ec86f0754e250bc64fa5dce6d381b6fc8f6f92
Qiong Wu <creasywuqiong@gmail.com>
2016-03-06 22:52:56 -0800

    sol of 2.42

--- ch2/2.42.rkt
+++ ch2/2.42.rkt
@@ -13,7 +13,21 @@
   (queen-cols board-size))
 
 
-(define (enumerate-interval 1 n)
-  (range 1 (+ n 1)))
+(define (enumerate-interval n m)
+  (range n (+ m 1)))
 
 (define (empty-board) '())
+
+(define (adjoin-position new-row new-col rest-of-queenss)
+  (cons (list new-row new-col) rest-of-queenss))
+
+(define (safe? new-col positions)
+  (letrec ((new (first positions))
+           (old (drop positions 1)))
+    (foldl (lambda (itr acc) (and (not (conflict? new itr)) acc)) #t old)))
+
+(define (conflict? x y)
+  (or (= (car x) (car y))
+      (= (cadr x) (cadr y))
+      ;; the mapping position for x in thr row of y
+      (= (+ (cadr x) (- (car y) (car x))) (cadr y))))
