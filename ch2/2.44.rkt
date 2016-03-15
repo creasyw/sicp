617756d0c3859707c94d25f9c801c20665bf47c0
Qiong Wu <creasywuqiong@gmail.com>
2016-03-15 14:33:43 -0700

    sol of 2.44

--- ch2/2.44.rkt
+++ ch2/2.44.rkt
@@ -1,18 +1,25 @@
 #lang racket
 
+(provide (all-define-out))
+
+
 (define wave2 (beside wave (filp-vert wave)))
 (define wave4 (below wave2 wave2))
+
 
 (define (flipped-pairs painter)
   (let ((painter2 (beside painter (filp-vert painter))))
     (below painter2 painter2)))
 
+
 (define wave4 (flipped-pairs wave))
+
 
 (define (right-split painter n)
   (if (= n 0) painter
       (let ((smaller (right-split painter (- n 1))))
         (beside painter (below smaller smaller)))))
+
 
 (define (corner-split painter n)
   (if (= n 0) painter
@@ -24,12 +31,14 @@
           (beside (below painter top-left)
                   (below bottom-right corner))))))
 
+
 (define (square-limit painter n)
   (letrec ((quarter (corner-split painter n))
            (half (beside (flip-horiz quarter) quarter)))
     (below (flip-vert half) half)))
 
+
 (define (up-split painter n)
   (if (= n 0) painter
       (let (smaller (up-split painter (- n 1)))
-        (below (beside smaller smaller) painter))))
+        (below painter (beside smaller smaller)))))
