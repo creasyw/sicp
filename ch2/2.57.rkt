8c17ac9114d503de45251ffd3319a3d67e044fe3
Qiong Wu <creasywuqiong@gmail.com>
2016-03-25 17:33:54 -0700

    sol of 2.57

--- ch2/2.57.rkt
+++ ch2/2.57.rkt
@@ -1,6 +1,12 @@
 #lang racket
 (require "symbolic_diff.rkt")
 
+;; The design of the program makes it possible to accept multiple
+;; input parameters via only changing the sum and product:
+;; The higher-level deriv has _no_ assumption about the number of
+;; input parameters, all operations are defined in a recursive manner.
+;; The lower-level of data representation also have no idea (and don't
+;; care about) how many operators are in a formula
 (define (new-sum . xs)
   (cond ((null? xs) 0)
         ((= (length xs) 1) (car xs))
