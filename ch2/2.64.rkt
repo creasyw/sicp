001ccdeebd5b2cc45dc32b03b71244f3eb86dd14
Qiong Wu <creasywuqiong@gmail.com>
2016-03-30 14:48:23 -0700

    sol of 2.64

--- ch2/2.64.rkt
+++ ch2/2.64.rkt
@@ -24,9 +24,15 @@
 ;; In every level, the helper function "partial-tree" break the list
 ;; into two (almost) even parts with length "left-size" and
 ;; "right-size". Specifically for the operations, it first recursively
-;; builds the left-tree, and then from the deapest level of left-tree
+;; builds the left-tree, and then from the deepest level of left-tree
 ;; it completes each corresponding right-tree part. The current root
 ;; will be returned as handler of the entire tree.
+;; Sidenote: it is clever to make the partial-tree has two input
+;; parameters, so that we can have the left tree first and then the
+;; right tree via playing with the index in the original list. Another
+;; methods is to use the append and build left- and right-tree at the
+;; same time. But that is similar to what is at 2.63. The latter has
+;; O(NlgN) complexity compared with O(N) for the former one.
 ;; b)
 ;; Because this algo will traverse each node once, the overall
 ;; complexity is O(N).
